42d16693bdb01343f07a0ad592086bf7b458a6d3
franx <miigo@duck.com>
2023-04-06 09:37:13 +0700

    dibagi 3 dan 5

--- src/tugas/pertemuan/lima/TigaDanLima.java
+++ src/tugas/pertemuan/lima/TigaDanLima.java
@@ -6,7 +6,13 @@ public class TigaDanLima {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Input sebuah bilangan: ");
+        System.out.print("Input sebuah bilangan: ");
+        int bil = scanner.nextInt();
 
+        if (bil % 3 == 0 && bil % 5 == 0){
+            System.out.println("Bilangan tersebut dapat dibagi angka 3 dan 5");
+        } else {
+            System.out.println("Bilangan tersebut tidak dapat dibagi angka 3 dan 5");
+        }
     }
 }
