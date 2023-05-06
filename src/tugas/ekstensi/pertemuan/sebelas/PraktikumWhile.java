d0477722db1788107093ecd12fbba2bc769af150
zero-two <miigo@duck.com>
2023-05-06 23:38:11 +0700

    loop nonsense

---
+++ src/tugas/ekstensi/pertemuan/sebelas/PraktikumWhile.java
@@ -0,0 +1,11 @@
+package tugas.ekstensi.pertemuan.sebelas;
+
+public class PraktikumWhile {
+    public static void main(String[] args) {
+        int x = 0;
+        while (x < 10) {
+            System.out.println("Nilai x = " + x);
+            x++;
+        }
+    }
+}
