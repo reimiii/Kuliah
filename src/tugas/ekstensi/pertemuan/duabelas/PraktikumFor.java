5cd1233e774a33e770f1c8358017e62b2705113f
zero-two <miigo@duck.com>
2023-05-07 23:14:57 +0700

    duabelas, for

---
+++ src/tugas/ekstensi/pertemuan/duabelas/PraktikumFor.java
@@ -0,0 +1,11 @@
+package tugas.ekstensi.pertemuan.duabelas;
+
+public class PraktikumFor {
+    public static void main(String[] args) {
+        int i;
+        for (i = 1; i <= 10; i++) {
+            // tubuh loop
+            System.out.println("JAVA");
+        }
+    }
+}
