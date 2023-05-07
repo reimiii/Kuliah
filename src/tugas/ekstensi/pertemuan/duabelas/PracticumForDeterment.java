9d4cfbfab621b2dc8a7a84d1280781f5fcad2a4b
zero-two <miigo@duck.com>
2023-05-07 23:30:50 +0700

    Increment and Determent

---
+++ src/tugas/ekstensi/pertemuan/duabelas/PracticumForDeterment.java
@@ -0,0 +1,11 @@
+package tugas.ekstensi.pertemuan.duabelas;
+
+public class PracticumForDeterment {
+    public static void main(String[] args) {
+        int i;
+        for (i = 20; i >= 0; i--) {
+            int x = i * 10;
+            System.out.println("Nilai ke-" + i + " : " + x);
+        }
+    }
+}
---
+++ src/tugas/ekstensi/pertemuan/duabelas/PraktikumForIncrement.java
@@ -0,0 +1,11 @@
+package tugas.ekstensi.pertemuan.duabelas;
+
+public class PraktikumForIncrement {
+    public static void main(String[] args) {
+        int i;
+        for (i = 1; i <= 10; i++) {
+            int x = i * 10;
+            System.out.println("Nilai ke-" + i + " : " + x);
+        }
+    }
+}
