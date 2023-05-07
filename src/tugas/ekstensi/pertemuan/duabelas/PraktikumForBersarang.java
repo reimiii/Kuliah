e1c2b3c107d83486bdd787c3cd3c9558b1ff4f20
zero-two <miigo@duck.com>
2023-05-07 23:34:51 +0700

    Bersarang

---
+++ src/tugas/ekstensi/pertemuan/duabelas/PraktikumForBersarang.java
@@ -0,0 +1,13 @@
+package tugas.ekstensi.pertemuan.duabelas;
+
+public class PraktikumForBersarang {
+    public static void main(String[] args) {
+        int i;
+        for (i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
