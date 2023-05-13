86790cae7ea1eb21da297ce46de3f1ac44ce4852
zero-two <miigo@duck.com>
2023-05-13 13:49:13 +0700

    PraktikumEmpatBelasCc

---
+++ src/tugas/ekstensi/pertemuan/tigabelas/latihan/PraktikumEmpatBelasCc.java
@@ -0,0 +1,14 @@
+package tugas.ekstensi.pertemuan.tigabelas.latihan;
+
+public class PraktikumEmpatBelasCc {
+    public static void main(String[] args) {
+        int arr[];
+        arr = new int[5];
+
+        arr[0] = 1;
+        arr[1] = 21;
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("arr[" + i + "] = " + arr[i]);
+        }
+    }
+}
